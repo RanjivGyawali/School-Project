0c3c64c823253d92e950c98381fd472b28bf5589
RanjivGyawali <86100867+RanjivGyawali@users.noreply.github.com>
2024-03-06 21:18:40 -0600

    Add files via upload

---
+++ homeapp.js
@@ -0,0 +1,34 @@
+function scrollToSection(targetId) {
+    // Get the target section's top offset
+    const targetSection = document.getElementById('targetId');
+    const targetOffset = targetSection.offsetTop;
+
+    // Scroll down to the target section
+    window.scrollTo({
+        top: targetOffset,
+        behavior: 'smooth' // Optional: Adds a smooth scrolling effect
+    });
+}
+
+function texas() {
+    // Get the target section's top offset
+    const targetSection = document.getElementById('target-section2');
+    const targetOffset = targetSection.offsetTop;
+
+    // Scroll down to the target section
+    window.scrollTo({
+        top: targetOffset,
+        behavior: 'smooth' // Optional: Adds a smooth scrolling effect
+    });
+}
+function about() {
+    // Get the target section's top offset
+    const targetSection = document.getElementById('target-section3');
+    const targetOffset = targetSection.offsetTop;
+
+    // Scroll down to the target section
+    window.scrollTo({
+        top: targetOffset,
+        behavior: 'smooth' // Optional: Adds a smooth scrolling effect
+    });
+}
